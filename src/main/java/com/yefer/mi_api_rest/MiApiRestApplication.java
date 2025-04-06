840f479ada5d133525dab62931f47515c5c12c4e
Yeffer Gomez <yegoalvear@gmail.com>
2025-04-06 16:13:34 -0500

    commit
    conexion a la base de datos

---
+++ src/main/java/com/yefer/mi_api_rest/MiApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.yefer.mi_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yefer/mi_api_rest/MiApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yefer.mi_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9881788c6e15979a392c39d500d570b22f92fd93
Yeffer Gomez <yegoalvear@gmail.com>
2025-04-16 13:59:51 -0500

    creacion de clase cliente

---
+++ src/main/java/com/yefer/mi_api_rest/model/entity/cliente.java
@@ -0,0 +1,9 @@
+package com.yefer.mi_api_rest.model.entity;
+
+public class cliente {
+ private Integer idcliente;
+ private String nombre;
+ private String apellido;
+ private String correo;
+ private String fechaderegistro;
+}
